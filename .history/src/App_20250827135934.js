eba389898a7b6cb8704aa5df5209b5ce2ccc9d33
SerejaTsoi20050419 <mistendo12345@gmail.com>
2025-09-03 09:49:12 +0900

    first commit

--- .history/src/App_20250827135934.js
+++ .history/src/App_20250827135934.js
@@ -4,7 +4,7 @@ import MovieSearch from './components/MovieSearch';
 function App() {
   return (
     <div style={{maxWidth: 900, margin: '32px auto', padding: 16, fontFamily: 'Arial, sans-serif'}}>
-      <h1>Movie Search </h1>
+      <h1>Movie Search (CRA)</h1>
       <MovieSearch />
     </div>
   );
